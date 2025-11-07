b6f44be5432e2467696d652c8448d2343bbf248a
moneya <moneya@users.noreply.github.com>
2025-11-07 17:38:33 +0100

    Refactor Livewire command imports and overrides

--- src/Support/ModularizedCommandsServiceProvider.php
+++ src/Support/ModularizedCommandsServiceProvider.php
@@ -31,88 +31,88 @@ use InterNACHI\Modular\Console\Commands\Make\MakeResource;
 use InterNACHI\Modular\Console\Commands\Make\MakeRule;
 use InterNACHI\Modular\Console\Commands\Make\MakeSeeder;
 use InterNACHI\Modular\Console\Commands\Make\MakeTest;
-use Livewire\Commands as Livewire;
+use Livewire\Features\SupportConsoleCommands\Commands as Livewire;
 
 class ModularizedCommandsServiceProvider extends ServiceProvider
 {
-	protected array $overrides = [
-		'command.cast.make' => MakeCast::class,
-		'command.controller.make' => MakeController::class,
-		'command.console.make' => MakeCommand::class,
-		'command.channel.make' => MakeChannel::class,
-		'command.event.make' => MakeEvent::class,
-		'command.exception.make' => MakeException::class,
-		'command.factory.make' => MakeFactory::class,
-		'command.job.make' => MakeJob::class,
-		'command.listener.make' => MakeListener::class,
-		'command.mail.make' => MakeMail::class,
-		'command.middleware.make' => MakeMiddleware::class,
-		'command.model.make' => MakeModel::class,
-		'command.notification.make' => MakeNotification::class,
-		'command.observer.make' => MakeObserver::class,
-		'command.policy.make' => MakePolicy::class,
-		'command.provider.make' => MakeProvider::class,
-		'command.request.make' => MakeRequest::class,
-		'command.resource.make' => MakeResource::class,
-		'command.rule.make' => MakeRule::class,
-		'command.seeder.make' => MakeSeeder::class,
-		'command.test.make' => MakeTest::class,
-		'command.component.make' => MakeComponent::class,
-		'command.seed' => SeedCommand::class,
-	];
-	
-	public function register(): void
-	{
-		// Register our overrides via the "booted" event to ensure that we override
-		// the default behavior regardless of which service provider happens to be
-		// bootstrapped first (this mostly matters for Livewire).
-		$this->app->booted(function() {
-			Artisan::starting(function(Application $artisan) {
-				$this->registerMakeCommandOverrides();
-				$this->registerMigrationCommandOverrides();
-				$this->registerLivewireOverrides($artisan);
-			});
-		});
-	}
-	
-	protected function registerMakeCommandOverrides()
-	{
-		foreach ($this->overrides as $alias => $class_name) {
-			$this->app->singleton($alias, $class_name);
-			$this->app->singleton(get_parent_class($class_name), $class_name);
-		}
-	}
-	
-	protected function registerMigrationCommandOverrides()
-	{
-		// Laravel 8
-		$this->app->singleton('command.migrate.make', function($app) {
-			return new MakeMigration($app['migration.creator'], $app['composer']);
-		});
-		
-		// Laravel 9
-		$this->app->singleton(OriginalMakeMigrationCommand::class, function($app) {
-			return new MakeMigration($app['migration.creator'], $app['composer']);
-		});
-	}
-	
-	protected function registerLivewireOverrides(Artisan $artisan)
-	{
-		// Don't register commands if Livewire isn't installed
-		if (! class_exists(Livewire\MakeCommand::class)) {
-			return;
-		}
-		
-		// Replace the resolved command with our subclass
-		$artisan->resolveCommands([MakeLivewire::class]);
-		
-		// Ensure that if 'make:livewire' or 'livewire:make' is resolved from the container
-		// in the future, our subclass is used instead
-		$this->app->extend(Livewire\MakeCommand::class, function() {
-			return new MakeLivewire();
-		});
-		$this->app->extend(Livewire\MakeLivewireCommand::class, function() {
-			return new MakeLivewire();
-		});
-	}
+    protected array $overrides = [
+        'command.cast.make' => MakeCast::class,
+        'command.controller.make' => MakeController::class,
+        'command.console.make' => MakeCommand::class,
+        'command.channel.make' => MakeChannel::class,
+        'command.event.make' => MakeEvent::class,
+        'command.exception.make' => MakeException::class,
+        'command.factory.make' => MakeFactory::class,
+        'command.job.make' => MakeJob::class,
+        'command.listener.make' => MakeListener::class,
+        'command.mail.make' => MakeMail::class,
+        'command.middleware.make' => MakeMiddleware::class,
+        'command.model.make' => MakeModel::class,
+        'command.notification.make' => MakeNotification::class,
+        'command.observer.make' => MakeObserver::class,
+        'command.policy.make' => MakePolicy::class,
+        'command.provider.make' => MakeProvider::class,
+        'command.request.make' => MakeRequest::class,
+        'command.resource.make' => MakeResource::class,
+        'command.rule.make' => MakeRule::class,
+        'command.seeder.make' => MakeSeeder::class,
+        'command.test.make' => MakeTest::class,
+        'command.component.make' => MakeComponent::class,
+        'command.seed' => SeedCommand::class,
+    ];
+
+    public function register(): void
+    {
+        // Register our overrides via the "booted" event to ensure that we override
+        // the default behavior regardless of which service provider happens to be
+        // bootstrapped first (this mostly matters for Livewire).
+        $this->app->booted(function () {
+            Artisan::starting(function (Application $artisan) {
+                $this->registerMakeCommandOverrides();
+                $this->registerMigrationCommandOverrides();
+                $this->registerLivewireOverrides($artisan);
+            });
+        });
+    }
+
+    protected function registerMakeCommandOverrides()
+    {
+        foreach ($this->overrides as $alias => $class_name) {
+            $this->app->singleton($alias, $class_name);
+            $this->app->singleton(get_parent_class($class_name), $class_name);
+        }
+    }
+
+    protected function registerMigrationCommandOverrides()
+    {
+        // Laravel 8
+        $this->app->singleton('command.migrate.make', function ($app) {
+            return new MakeMigration($app['migration.creator'], $app['composer']);
+        });
+
+        // Laravel 9
+        $this->app->singleton(OriginalMakeMigrationCommand::class, function ($app) {
+            return new MakeMigration($app['migration.creator'], $app['composer']);
+        });
+    }
+
+    protected function registerLivewireOverrides(Artisan $artisan)
+    {
+        // Don't register commands if Livewire isn't installed
+        if (! class_exists(Livewire\MakeCommand::class)) {
+            return;
+        }
+
+        // Replace the resolved command with our subclass
+        $artisan->resolveCommands([MakeLivewire::class]);
+
+        // Ensure that if 'make:livewire' or 'livewire:make' is resolved from the container
+        // in the future, our subclass is used instead
+        $this->app->extend(Livewire\MakeCommand::class, function () {
+            return new MakeLivewire();
+        });
+        $this->app->extend(Livewire\MakeLivewireCommand::class, function () {
+            return new MakeLivewire();
+        });
+    }
 }
